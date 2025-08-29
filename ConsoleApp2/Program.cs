e39c3db55c2657c1c1f2ec59d73c1e18731cea6d
Almighty1991 <janisrachow@gmail.de>
2025-08-29 09:22:04 +0200

    commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("lol");
+            Console.WriteLine("lol123");
         }
     }
 }
